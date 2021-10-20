935ed0a6dd402cd9a0623d1a9da5370d622296dc
aamirshaikh7 <aamirshaikh3232@gmail.com>
2021-10-21 01:16:38 +0530

    Dependency Injection using ApplicationContext

---
+++ SpringIoC/src/main/java/com/demo/ApplicationContext/Device.java
@@ -0,0 +1,42 @@
+package com.demo.ApplicationContext;
+
+import org.springframework.context.annotation.Bean;
+
+public class Device {
+    private int id;
+    private String device;
+    private String description;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDevice() {
+        return device;
+    }
+
+    public void setDevice(String device) {
+        this.device = device;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Device{" +
+                "id=" + id +
+                ", device='" + device + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
