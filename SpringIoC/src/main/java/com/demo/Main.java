c06b10678b66e8c7bb0ad672b5db5718f01bb0ee
aamirshaikh7 <aamirshaikh3232@gmail.com>
2021-10-20 13:20:00 +0530

    Dependency Injection using BeanFactory container

---
+++ SpringIoC/src/main/java/com/demo/Main.java
@@ -0,0 +1,26 @@
+package com.demo;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+public class Main {
+    public static void main(String[] args) {
+        Device constructedObject = new Device();
+
+        System.out.println("Constructed Object : " + constructedObject);
+
+        System.out.println();
+
+        System.out.println("BeanFactory Container");
+
+        Resource resource = new ClassPathResource("applicationContext.xml");
+
+        BeanFactory beanFactory = new XmlBeanFactory(resource);
+
+        Device beanObject = (Device) beanFactory.getBean("device");
+
+        System.out.println("Bean Object : " + beanObject);
+    }
+}
