5f7c870c95fba677761ad1379c08742ba4edc005
aamirshaikh7 <aamirshaikh3232@gmail.com>
2021-10-21 12:59:53 +0530

    ApplicationContext - populating Beans in applicationContext.xml file using property

--- SpringIoC/src/main/java/com/demo/ApplicationContext/Main.java
+++ SpringIoC/src/main/java/com/demo/ApplicationContext/Main.java
@@ -9,14 +9,6 @@ public class Main {
         Device device1 = (Device) applicationContext.getBean("device1");
         Device device2 = (Device) applicationContext.getBean("device2");
 
-        device1.setId(1);
-        device1.setDevice("Device 1");
-        device1.setDescription("Description 1");
-
-        device2.setId(2);
-        device2.setDevice("Device 2");
-        device2.setDescription("Description 2");
-
         System.out.println("ApplicationContext : " + device1 + " " + device2);
 
         applicationContext.close();
