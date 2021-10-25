961f2478ff88360a1ad4c1fcf4adf51d15886890
Aamir shaikh <aamirshaikh3232@gmail.com>
2021-10-25 18:33:12 +0530

    Generating Product POJO Class

---
+++ SpringAspectOrientedProgramming(AOP)/src/main/java/com/demo/Pointcuts/Product.java
@@ -0,0 +1,53 @@
+package com.demo.Pointcuts;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Product {
+    private int productId;
+    private String productName;
+    private float productPrice;
+    private int productQuantity;
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public float getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(float productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public int getProductQuantity() {
+        return productQuantity;
+    }
+
+    public void setProductQuantity(int productQuantity) {
+        this.productQuantity = productQuantity;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "productId=" + productId +
+                ", productName='" + productName + '\'' +
+                ", productPrice=" + productPrice +
+                ", productQuantity=" + productQuantity +
+                '}';
+    }
+}
