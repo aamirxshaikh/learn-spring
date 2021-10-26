fdf7c1c6258e788052c2aa921af23e9eafd37af7
Aamir shaikh <aamirshaikh3232@gmail.com>
2021-10-26 16:03:50 +0530

    Running Aspect-oriented programs

--- SpringAspectOrientedProgramming(AOP)/src/main/java/com/demo/Main.java
+++ SpringAspectOrientedProgramming(AOP)/src/main/java/com/demo/Main.java
@@ -1,7 +1,18 @@
 package com.demo;
 
+import com.demo.Pointcuts.Product;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
 
+        Product product = (Product) applicationContext.getBean("product");
+
+        product.printProductDetails();
+
+        System.out.println(product.toString());
+        System.out.println(product.getProductName());
     }
 }
