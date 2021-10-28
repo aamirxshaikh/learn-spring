4314883ac5e2f90b2975f1a2399eec8c3ff2cbc6
Aamir shaikh <aamirshaikh3232@gmail.com>
2021-10-28 13:53:46 +0530

    Using Before and After Advices

--- SpringAspectOrientedProgramming(AOP)/src/main/java/com/demo/AdvicesAOP/AspectConfig.java
+++ SpringAspectOrientedProgramming(AOP)/src/main/java/com/demo/AdvicesAOP/AspectConfig.java
@@ -1,9 +1,27 @@
 package com.demo.AdvicesAOP;
 
+import com.demo.AdvicesAOP.Advices.Order;
 import org.aspectj.lang.JoinPoint;
+import org.springframework.context.ApplicationContext;
 
 public class AspectConfig {
+    private Order getOrder() {
+        ApplicationContext applicationContext = ApplicationContextWrapper.getApplicationContext();
+
+        return (Order) applicationContext.getBean("order");
+    }
+
     public void printMethodName(JoinPoint joinPoint) {
         System.out.println("Called by : " + joinPoint.getSignature().getName());
+    }
+
+    public void printOrderValue() {
+        Order order = getOrder();
+
+        System.out.println("============================");
+
+        System.out.println("Order Total : " + order.getOrderValue());
+
+        System.out.println("============================");
     }
 }
--- SpringAspectOrientedProgramming(AOP)/src/main/java/com/demo/AdvicesAOP/Main.java
+++ SpringAspectOrientedProgramming(AOP)/src/main/java/com/demo/AdvicesAOP/Main.java
@@ -1,4 +1,14 @@
 package com.demo.AdvicesAOP;
 
+import com.demo.AdvicesAOP.Advices.Order;
+import org.springframework.context.ApplicationContext;
+
 public class Main {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = ApplicationContextWrapper.getApplicationContext();
+
+        Order order = (Order) applicationContext.getBean("order");
+
+        order.displayOrderProducts();
+    }
 }
