af1a048ff56a06ac06264c4e9412c199239d7ff8
Aamir shaikh <aamirshaikh3232@gmail.com>
2021-11-01 23:17:05 +0530

    Persisting Data in MySQL Database with Hibernate

--- SpringJPAHibernate/src/main/java/com/demo/App.java
+++ SpringJPAHibernate/src/main/java/com/demo/App.java
@@ -1,13 +1,33 @@
 package com.demo;
 
-/**
- * Hello world!
- *
- */
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Configuration configuration = new Configuration();
+
+        configuration = configuration.configure("META-INF/hibernate.cfg.xml");
+
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+
+        Session session = sessionFactory.openSession();
+
+        Employee employee1 = new Employee(100, "Abc", "Xyz", "Associate", 9999);
+        Employee employee2 = new Employee(100, "Pqr", "Xyz", "Associate", 8999);
+
+        Transaction transaction = session.beginTransaction();
+
+        session.save(employee1);
+        session.save(employee2);
+
+        transaction.commit();
+
+        session.close();
+        sessionFactory.close();
     }
 }
