be254d4e21b3afd0a25f847dec2c42c43011ce4d
Aamir shaikh <aamirshaikh3232@gmail.com>
2021-11-01 21:34:36 +0530

    Defining Hibernate Configurations

---
+++ SpringJPAHibernate/src/main/java/com/demo/Employee.java
@@ -0,0 +1,60 @@
+package com.demo;
+
+public class Employee {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String job;
+    private double salary;
+
+    public Employee() {
+    }
+
+    public Employee(int id, String firstName, String lastName, String job, double salary) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.job = job;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
