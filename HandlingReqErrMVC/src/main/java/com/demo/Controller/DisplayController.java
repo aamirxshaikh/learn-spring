77c473f8ce41378bcbc06190bed3cee02b459eb5
Aamir shaikh <aamirshaikh3232@gmail.com>
2021-11-06 00:48:27 +0530

    Setting up DisplayController to Handle Web Requests

---
+++ HandlingReqErrMVC/src/main/java/com/demo/Controller/DisplayController.java
@@ -0,0 +1,23 @@
+package com.demo.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class DisplayController {
+    @RequestMapping(value = "/index")
+    public ModelAndView index(Model model) {
+        model.addAttribute("message", "index");
+
+        return new ModelAndView("index.jsp");
+    }
+
+    @RequestMapping(value = "/test")
+    public ModelAndView test(Model model) {
+        model.addAttribute("message","test");
+
+        return new ModelAndView("index.jsp");
+    }
+}
