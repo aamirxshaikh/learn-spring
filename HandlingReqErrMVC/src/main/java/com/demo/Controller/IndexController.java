9f62bafe3dd7cf488a31de2b68df8ec8ac33d101
Aamir shaikh <aamirshaikh3232@gmail.com>
2021-11-07 23:38:43 +0530

    Configuring Multiple Controllers

--- HandlingReqErrMVC/src/main/java/com/demo/Controller/IndexController.java
+++ HandlingReqErrMVC/src/main/java/com/demo/Controller/IndexController.java
@@ -4,14 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class DisplayController {
+public class IndexController {
     @RequestMapping(value = "/index")
     public String index() {
         return "index_page";
-    }
-
-    @RequestMapping(value = "/test")
-    public String test() {
-        return "test_page";
     }
 }
