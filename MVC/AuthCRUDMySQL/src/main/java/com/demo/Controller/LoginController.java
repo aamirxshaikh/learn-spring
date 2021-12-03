139704bf2ffe907b4adf9e1c37b489c745892b11
aamirshaikh7 <aamirshaikh3232@gmail.com>
2021-12-03 14:32:02 +0530

    Performing login and register in Login and Register Controller

--- MVC/AuthCRUDMySQL/src/main/java/com/demo/Controller/LoginController.java
+++ MVC/AuthCRUDMySQL/src/main/java/com/demo/Controller/LoginController.java
@@ -1,6 +1,7 @@
 package com.demo.Controller;
 
 import com.demo.Model.Login;
+import com.demo.Model.User;
 import com.demo.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -15,11 +16,33 @@ import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class LoginController {
+    @Autowired
+    UserService userService;
+
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public ModelAndView show(HttpServletRequest request, HttpServletResponse response) {
         ModelAndView modelAndView = new ModelAndView("login");
 
         modelAndView.addObject("login", new Login());
+
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public ModelAndView login(HttpServletRequest request, HttpServletResponse response, @ModelAttribute("login") Login login, Model model) {
+        ModelAndView modelAndView = null;
+
+        User user = userService.validateUser(login);
+
+        if(user != null) {
+            model.addAttribute("user", user);
+
+            modelAndView = new ModelAndView("dashboard");
+        } else {
+            modelAndView = new ModelAndView("login");
+
+            modelAndView.addObject("message", "ID or Password is incorrect. Please try again !");
+        }
 
         return modelAndView;
     }
--- MVC/AuthCRUDMySQL/src/main/java/com/demo/Controller/RegisterController.java
+++ MVC/AuthCRUDMySQL/src/main/java/com/demo/Controller/RegisterController.java
@@ -1,16 +1,26 @@
 package com.demo.Controller;
 
+import com.demo.Model.Login;
 import com.demo.Model.User;
+import com.demo.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 @Controller
 public class RegisterController {
+    @Autowired
+    UserService userService;
+
     @RequestMapping(value = "/register", method = RequestMethod.GET)
     public ModelAndView show(HttpServletRequest request, HttpServletResponse response) {
         ModelAndView modelAndView = new ModelAndView("register");
@@ -18,5 +28,16 @@ public class RegisterController {
         modelAndView.addObject("user", new User());
 
         return modelAndView;
+    }
+
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public String register(@Valid @ModelAttribute("user") User user, BindingResult bindingResult) {
+        if(bindingResult.hasErrors()) {
+            return "register";
+        }
+
+        userService.register(user);
+
+        return "redirect:login";
     }
 }
