4e03423f2684ec05c28f7df0306a3797b6fd8b89
Aamir shaikh <aamirshaikh3232@gmail.com>
2021-12-10 01:04:39 +0530

    Connecting to MySQL Database and Creating Table

--- Spring Data/SpringDataJDBC/src/main/java/com/demo/SpringDataJDBC/SpringDataJdbcApplication.java
+++ Spring Data/SpringDataJDBC/src/main/java/com/demo/SpringDataJDBC/SpringDataJdbcApplication.java
@@ -1,13 +1,24 @@
 package com.demo.SpringDataJDBC;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
-public class SpringDataJdbcApplication {
+public class SpringDataJdbcApplication implements CommandLineRunner {
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDataJdbcApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		String sql = "CREATE TABLE IF NOT EXISTS employees (id int PRIMARY KEY, first_name varchar(100), last_name varchar(100), email varchar(100))";
+
+		jdbcTemplate.execute(sql);
+	}
 }
