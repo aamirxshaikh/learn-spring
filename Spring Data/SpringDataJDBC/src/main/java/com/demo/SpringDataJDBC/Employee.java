f2c1015cad228aea7b4fbf1476c7bc81a9d14682
Aamir shaikh <aamirshaikh3232@gmail.com>
2021-12-10 21:18:01 +0530

    Mapping Class to a Table

---
+++ Spring Data/SpringDataJDBC/src/main/java/com/demo/SpringDataJDBC/Employee.java
@@ -0,0 +1,77 @@
+package com.demo.SpringDataJDBC;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Table("employees")
+public class Employee {
+    @Id
+    @Column("id")
+    private Integer id;
+
+    @Column("first_name")
+    private String firstName;
+
+    @Column("last_name")
+    private String lastName;
+
+    @Column("email")
+    private String email;
+
+    public Employee() {
+    }
+
+    public Employee(Integer id, String firstName, String lastName, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    static Employee create(Integer id, String firstName, String lastName, String email) {
+        return new Employee(id, firstName, lastName, email);
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
