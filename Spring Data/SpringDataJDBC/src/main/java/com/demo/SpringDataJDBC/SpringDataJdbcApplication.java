cc8c0e1535b1bbe3ce85150cf5a7fc6999195c10
Aamir shaikh <aamirshaikh3232@gmail.com>
2021-12-10 20:56:39 +0530

    Performing CRUD Operations with JdbcTemplate

--- Spring Data/SpringDataJDBC/src/main/java/com/demo/SpringDataJDBC/SpringDataJdbcApplication.java
+++ Spring Data/SpringDataJDBC/src/main/java/com/demo/SpringDataJDBC/SpringDataJdbcApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jdbc.core.JdbcTemplate;
 
+import java.util.List;
+
 @SpringBootApplication
 public class SpringDataJdbcApplication implements CommandLineRunner {
 	@Autowired
@@ -17,8 +19,47 @@ public class SpringDataJdbcApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		String sql = "CREATE TABLE IF NOT EXISTS employees (id int PRIMARY KEY, first_name varchar(100), last_name varchar(100), email varchar(100))";
+//		create
 
-		jdbcTemplate.execute(sql);
+//		String sql = "CREATE TABLE IF NOT EXISTS employees (id int PRIMARY KEY, first_name varchar(100), last_name varchar(100), email varchar(100))";
+//
+//		jdbcTemplate.execute(sql);
+
+//		insert
+
+//		String sql = "INSERT INTO employees VALUES (?, ?, ?, ?)";
+//
+//		jdbcTemplate.update(sql, 1, "John", "Doe", "jdoe@test.com");
+//		jdbcTemplate.update(sql, 2, "Jane", "Doe", "janedoe@test.com");
+
+//		update
+
+//		String sql = "UPDATE employees SET email = ? WHERE id = ?";
+//
+//		jdbcTemplate.update(sql, "johndoe@test.com", 1);
+
+//		select
+
+//		String sql = "SELECT * FROM employees";
+//
+//		List employees = jdbcTemplate.queryForList(sql);
+//
+//		for (Object employee : employees) {
+//			System.out.println(employee);
+//		}
+
+//		delete
+
+		String sql = "DELETE FROM employees WHERE id = ?";
+
+		jdbcTemplate.update(sql, 1);
+
+		sql = "SELECT * FROM employees";
+
+		List employees = jdbcTemplate.queryForList(sql);
+
+		for (Object employee : employees) {
+			System.out.println(employee);
+		}
 	}
 }
