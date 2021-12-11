9a6a21d37f1b436e2645d15fdb647f6b35660065
Aamir shaikh <aamirshaikh3232@gmail.com>
2021-12-11 19:10:55 +0530

    Setting-up New Spring Boot Project for CrudRepository and Specifying application.properties

---
+++ Spring Data/SpringDataJDBCCrudRepository/src/main/java/com/demo/springdatajdbccrudrepository/SpringDataJdbcCrudRepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.demo.springdatajdbccrudrepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJdbcCrudRepositoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataJdbcCrudRepositoryApplication.class, args);
+    }
+
+}
