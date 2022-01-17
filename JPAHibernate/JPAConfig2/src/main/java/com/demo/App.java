d2d55842369543c047cefdf7c9a99c6a815a762c
Aamir shaikh <aamirshaikh3232@gmail.com>
2022-01-17 23:39:32 +0530

    Creating Owner Entity

--- JPAHibernate/JPAConfig2/src/main/java/com/demo/App.java
+++ JPAHibernate/JPAConfig2/src/main/java/com/demo/App.java
@@ -3,6 +3,7 @@ package com.demo;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import java.util.GregorianCalendar;
 
 /**
  * Hello world!
@@ -24,6 +25,13 @@ public class App
 
             entityManager.persist(car1);
             entityManager.persist(car2);
+
+            Owner owner1 = new Owner("Aamir", new GregorianCalendar(2000, 1, 3).getTime());
+            Owner owner2 = new Owner("Aamir", new GregorianCalendar(2000, 2, 3).getTime());
+
+
+            entityManager.persist(owner1);
+            entityManager.persist(owner2);
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
---
+++ JPAHibernate/JPAConfig2/src/main/java/com/demo/Owner.java
@@ -0,0 +1,48 @@
+package com.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+public class Owner {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private Date dob;
+
+    public Owner() {
+    }
+
+    public Owner(String name, Date dob) {
+        this.name = name;
+        this.dob = dob;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+}
