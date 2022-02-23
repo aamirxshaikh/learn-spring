8cbb020dd02c8144bdcd4a0e700c055e7cdd70d6
Aamir shaikh <aamirshaikh3232@gmail.com>
2022-02-23 16:02:46 +0530

    Retrieving Entities

--- JPAHibernate/JPAHibernateInheritedClasses/src/main/java/com/demo/App.java
+++ JPAHibernate/JPAHibernateInheritedClasses/src/main/java/com/demo/App.java
@@ -20,50 +20,50 @@ public class App
         try {
             entityManager.getTransaction().begin();
 
-            Department it = new Department("IT");
+//            Department it = new Department("IT");
+//
+//            Employee aamir = new Employee("Aamir");
+//
+//            FullTimeEmployee john = new FullTimeEmployee("John", 9999);
+//            FullTimeEmployee jack = new FullTimeEmployee("Jack", 8999);
+//
+//            ContractEmployee jane = new ContractEmployee("Jane", 20);
+//            ContractEmployee jim = new ContractEmployee("Jim", 30);
+//
+//            it.addEmployee(aamir);
+//            it.addEmployee(john);
+//            it.addEmployee(jack);
+//            it.addEmployee(jane);
+//            it.addEmployee(jim);
+//
+//            entityManager.persist(it);
 
-            Employee aamir = new Employee("Aamir");
+            Department getIT = entityManager.find(Department.class, 1);
 
-            FullTimeEmployee john = new FullTimeEmployee("John", 9999);
-            FullTimeEmployee jack = new FullTimeEmployee("Jack", 8999);
+            System.out.println("Department");
+            System.out.println(getIT);
 
-            ContractEmployee jane = new ContractEmployee("Jane", 20);
-            ContractEmployee jim = new ContractEmployee("Jim", 30);
+            Employee getAamir = entityManager.find(Employee.class, 3);
 
-            it.addEmployee(aamir);
-            it.addEmployee(john);
-            it.addEmployee(jack);
-            it.addEmployee(jane);
-            it.addEmployee(jim);
+            System.out.println("Employee");
+            System.out.println(getAamir);
 
-            entityManager.persist(it);
+            Employee getJohn = entityManager.find(FullTimeEmployee.class, 1);
 
-//            Department getIT = entityManager.find(Department.class, 1);
+            System.out.println("FullTimeEmployee");
+            System.out.println(getJohn);
 //
-//            System.out.println("Department");
-//            System.out.println(getIT);
-//
-//            Employee getAamir = entityManager.find(Employee.class, 3);
-//
-//            System.out.println("Employee");
-//            System.out.println(getAamir);
-//
-//            Employee getJohn = entityManager.find(FullTimeEmployee.class, 1);
-//
-//            System.out.println("FullTimeEmployee");
-//            System.out.println(getJohn);
-//
-//            Employee getJane = entityManager.find(ContractEmployee.class, 2);
-//
-//            System.out.println("ContractEmployee");
-//            System.out.println(getJane);
-//
-//            List<Employee> employees = (List<Employee>) entityManager.createQuery("from Employees", Employee.class).getResultList();
-//
-//            System.out.println("All Employees");
-//            for (Employee e: employees) {
-//                System.out.println(e);
-//            }
+            Employee getJane = entityManager.find(ContractEmployee.class, 2);
+
+            System.out.println("ContractEmployee");
+            System.out.println(getJane);
+
+            List<Employee> employees = (List<Employee>) entityManager.createQuery("from Employees", Employee.class).getResultList();
+
+            System.out.println("All Employees");
+            for (Employee e: employees) {
+                System.out.println(e);
+            }
         } catch(Exception e) {
             e.printStackTrace();
         } finally {
