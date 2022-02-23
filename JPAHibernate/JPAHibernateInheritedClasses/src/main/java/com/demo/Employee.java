4f9f6584091edc9083e501fe1f62843eef35dd12
Aamir shaikh <aamirshaikh3232@gmail.com>
2022-02-23 15:55:35 +0530

    Inheritance Type JOINED

--- JPAHibernate/JPAHibernateInheritedClasses/src/main/java/com/demo/Employee.java
+++ JPAHibernate/JPAHibernateInheritedClasses/src/main/java/com/demo/Employee.java
@@ -4,7 +4,7 @@ import javax.persistence.*;
 import java.io.Serializable;
 
 @Entity(name = "Employees")
-@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@Inheritance(strategy = InheritanceType.JOINED)
 public class Employee implements Serializable {
     private static final long serialVersionUID = 1L;
 
